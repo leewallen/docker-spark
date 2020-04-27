e311a5e78cd2583c0cad2ce3bd3a81c7c12c2e4b
Lee Wallen <fayq@M-C02V11NVHTDF.nordstrom.net>
2020-04-27 11:45:28 -0700

    update example so it will create data directory if it doesn't exist, and make script executable.

--- examples/copy-count-letters.sh
+++ examples/copy-count-letters.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 tar -xvzf count-letters.tgz
+
+mkdir -p ../data
+mkdir -p ../jars
+mkdir -p ../scripts
+
 cp ./count-letters/data/*.* ../data/
 cp ./count-letters/jars/*.* ../jars/
 cp ./count-letters/scripts/*.* ../scripts/
-
+chmod +x ../scripts/run-example.sh
