28c9f6d744a2c10064b3459431caab699434310c
Lee Wallen <fayq@M-C02V11NVHTDF.nordstrom.net>
2020-04-30 08:20:32 -0700

    add powershell script for example, add .gitattributes for line feed handling

---
+++ examples/copy-count-letters.ps1
@@ -0,0 +1,9 @@
+tar -xvzf count-letters.tgz
+
+mkdir -p ../data
+mkdir -p ../jars
+mkdir -p ../scripts
+
+cp ./count-letters/data/*.* ../data/
+cp ./count-letters/jars/*.* ../jars/
+cp ./count-letters/scripts/*.* ../scripts/
