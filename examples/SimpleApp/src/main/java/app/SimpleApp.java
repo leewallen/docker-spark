f325af7200365035f951b23be2b8486dea3836e5
lwallen <lwallen@gettyimages.com>
2021-01-26 21:52:24 -0800

    changed example but switched back - left unused var, and also left out lower casing source string before check

--- examples/SimpleApp/src/main/java/app/SimpleApp.java
+++ examples/SimpleApp/src/main/java/app/SimpleApp.java
@@ -1,21 +1,14 @@
 package app;
 
-import org.apache.spark.api.java.function.*;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.io.FileNotFoundException;
-import java.io.File;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.*;
+import java.util.Locale;
 
 public class SimpleApp {
   public static void main(String[] args) {
-    SimpleApp app = new SimpleApp();
     String targetFile = ""; // Should be some file on your system
     if (args.length > 0) {
       targetFile = args[0];
@@ -28,9 +21,9 @@ public class SimpleApp {
     Dataset<String> logData = spark.read().textFile(targetFile);
 
     System.out.printf("Lines with a   : %s\nLines with b   : %s\nLines with java: %s\n",
-            logData.filter((FilterFunction<String>) s -> s.contains("a")).count(),
-            logData.filter((FilterFunction<String>) s -> s.contains("b")).count(),
-            logData.filter((FilterFunction<String>) s -> s.contains("java")).count()
+      logData.filter((FilterFunction<String>) s -> s.toLowerCase().contains("a")).count(),
+      logData.filter((FilterFunction<String>) s -> s.toLowerCase().contains("b")).count(),
+      logData.filter((FilterFunction<String>) s -> s.toLowerCase().contains("java")).count()
     );
 
     spark.stop();
